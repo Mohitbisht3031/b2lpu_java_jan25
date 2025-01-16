ea17e8d427b7d51fe1b4b91987cf52217c6eb76e
Mohit Bisht <mohitbisht@192.168.1.6>
2025-01-16 20:48:00 +0530

    Day1

---
+++ Day1/welcome.java
@@ -0,0 +1,7 @@
+package Day1;
+
+public class welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the core java class!");    
+    }
+}
---
+++ Day1/welcomeInvoker.java
@@ -0,0 +1,9 @@
+package Day1;
+import Day1.welcome;
+
+public class welcomeInvoker {
+    public static void main(String[] args) {
+        System.out.println("Inside the welcome invoker class!");
+        welcome.main(args);
+    }
+}
