54990a6167752d19212a22a7ddfd6409f7cc64b2
Mohit Bisht <mohitbisht@192.168.1.23>
2025-01-23 20:56:02 +0530

    Day4

---
+++ Day4/User.java
@@ -0,0 +1,21 @@
+package Day4;
+
+public class User{
+    String name;
+    String phnNo;
+    String Pin;
+    Double balance;
+
+    String getName(){
+        return name;
+    }
+    String getPhnNo(){
+        return phnNo;
+    }
+    double getBalance(){
+        return balance;
+    }
+
+    // setter methods for all 4
+
+}
---
+++ Day4/wrapper.java
@@ -0,0 +1,9 @@
+package Day4;
+
+public class wrapper {
+    public static void main(String[] args) {
+        Integer i = 10; // autoboxing
+        var i2 = "Mohit";
+        System.out.println(i2);
+    }
+}
